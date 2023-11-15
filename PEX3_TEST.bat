142e3a71950eec3aadd6f4025f5ceb554bb90d2e
KaylebKlapp <90409926+KaylebKlapp@users.noreply.github.com>
2023-11-14 21:28:03 -0700

    Add files via upload
    Batch file

--- PEX3_TEST.bat
+++ PEX3_TEST.bat
@@ -7,100 +7,59 @@ type NUL>%file%
 :: ----------------------------------------
 :: TITLE
 :: ----------------------------------------
-echo PEX TEST CASES (C1C Starkey) >> %file%
+echo PEX TEST CASES (C1C Starkey, C1C Klapp) >> %file%
 
-:: ----------------------------------------
-:: GOOD EXAMPLES
-:: ----------------------------------------
-::echo Testing Identifiers >> %file%
-::bin\Debug\ConsoleApplication.exe testcases\pex1\test1.txt >> %file%
-::echo. >> %file%
-
-echo Testing Actual Parameters >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\actual_params_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Arithmetic >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\arithmetic_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Assignment >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\assign_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Boolean >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\boolean_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Declarations >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\declaration_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Formal Parameters >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\formal_params_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Functions >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\functions_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing If Statements >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\if_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing Main Function >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\main_correct.txt >> %file%
-echo. >> %file%
-
-echo Testing While Loops >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\while_correct.txt >> %file%
-echo. >> %file%
 :: ----------------------------------------
 :: BAD EXAMPLES
 :: ----------------------------------------
 ::echo Running Incorrect Test Cases >> %file%
 ::echo. >> %file%
 
-echo Running Inccorect Test Cases >> %file%
-echo. >> %file%
-echo Testing Actual Parameters >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\actual_params_incorrect.txt >> %file%
+echo Testing Bad If/While Loops>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\if_while_incorrect.txt >> %file%
 echo. >> %file%
 
-echo Testing Arithmetic >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\arithmetic_incorrect.txt >> %file%
+echo Testing Bad Declaration/ Assignment Statements>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\declaration_assignment_incorrect.txt >> %file%
 echo. >> %file%
 
-echo Testing Assignment >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\assign_incorrect.txt >> %file%
+echo Testing Bad Constant Assignment>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\constant_assignment_incorrect.txt >> %file%
 echo. >> %file%
 
-echo Testing Boolean >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\boolean_incorrect.txt >> %file%
+echo Testing Bad Boolean Operations>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\boolean_operation_incorrect.txt >> %file%
 echo. >> %file%
 
-echo Testing Declarations >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\declaration_incorrect.txt >> %file%
+echo Testing Bad Arithmetic>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\arithmetic_incorrect.txt >> %file%
 echo. >> %file%
 
-echo Testing Formal Parameters >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\formal_params_incorrect.txt >> %file%
+
+:: ----------------------------------------
+:: GOOD EXAMPLES
+:: ----------------------------------------
+
+
+echo Testing Good If/While Loops>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\if_while_correct.txt >> %file%
 echo. >> %file%
 
-echo Testing Functions >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\functions_incorrect.txt >> %file%
+echo Testing Good Declaration Statements>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\declaration_assignment_correct.txt >> %file%
 echo. >> %file%
 
-echo Testing If Statements >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\if_incorrect.txt >> %file%
+echo Testing Good Constant Assignment>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\constant_assignment_correct.txt >> %file%
 echo. >> %file%
 
-echo Testing Main Function >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\main_incorrect.txt >> %file%
+echo Testing Good Boolean Operations>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\boolean_operation_correct.txt >> %file%
 echo. >> %file%
 
-echo Testing While Loops >> %file%
-bin\Debug\ConsoleApplication.exe testcases\pex2\while_incorrect.txt >> %file%
+echo Testing Good Arithmetic>> %file%
+bin\Debug\ConsoleApplication.exe testcases\pex3\arithmetic_correct.txt >> %file%
 echo. >> %file%
+
 
 pause
